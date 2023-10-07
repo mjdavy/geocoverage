936d4b28f8f15fb13d24029d188993f7d8112e32
Martin Davy <martin.davy@gmail.com>
2023-10-07 12:16:54 -0400

    Initial Commit

---
+++ GeoCoverage/GeoCoverageApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoCoverageApp.swift
+//  GeoCoverage
+//
+//  Created by Martin Davy on 10/7/23.
+//
+
+import SwiftUI
+
+@main
+struct GeoCoverageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
